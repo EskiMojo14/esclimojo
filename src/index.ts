a0c76545dddc17e54a800d5c2c089532c651f5d0
EskiMojo14 <ben.j.durrant@gmail.com>
2024-01-10 00:29:18 +0000

    pluralise name

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { program } from "commander";
 import { name, version, description } from "../package.json";
 import "./commands/add-entrypoints";
-import "./commands/copy-template";
+import "./commands/copy-templates";
 import "./commands/init";
 
 program.name(name).version(version).description(description);
