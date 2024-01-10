4c5ff2369865c4c3df514a10d15508a89fa1324f
EskiMojo14 <ben.j.durrant@gmail.com>
2024-01-10 00:18:19 +0000

    Support pm-specific templates in copy-templates

--- src/commands/copy-template.ts
+++ src/commands/copy-template.ts
@@ -6,7 +6,14 @@ import picocolors from "picocolors";
 import { object, optional, boolean, parse, string, array } from "valibot";
 import { __dirname } from "../constants";
 import { ensureNotCancelled } from "../lib/clack";
+import type { SupportedManager } from "../lib/package-managers";
+import { supportedManagers } from "../lib/package-managers";
 import { copyTemplate } from "../lib/templates";
+
+interface TemplateDesc {
+  filename: string;
+  packageManager?: SupportedManager;
+}
 
 const copyTemplateOptions = object({
   yes: optional(boolean()),
@@ -15,37 +22,103 @@ const copyTemplateOptions = object({
 program
   .command("copy-templates")
   .option("-y, --yes", "override existing file without asking")
-  .argument("[filenames...]")
+  .argument(
+    "[filenames...]",
+    "file paths - package specific templates need to be prefixed e.g. yarn:.yarnrc.yml"
+  )
   .action(async (files, options) => {
     intro("Copy templates");
-    const templates = await readdir(join(__dirname, "templates"));
-    let filenames = parse(optional(array(string())), files);
+    const templates = (
+      await readdir(join(__dirname, "templates"), {
+        recursive: true,
+      })
+    ).map(
+      (filename): TemplateDesc => ({
+        filename,
+      })
+    );
+    for (const packageManager of supportedManagers) {
+      try {
+        const pmTemplates = await readdir(
+          join(__dirname, "pm-templates", packageManager),
+          { recursive: true }
+        );
+        templates.push(
+          ...pmTemplates.map((filename) => ({
+            filename,
+            packageManager,
+          }))
+        );
+      } catch {
+        // no specific templates
+      }
+    }
+    const filenames = parse(optional(array(string())), files);
     const { yes } = parse(copyTemplateOptions, options);
 
+    let processed: Array<TemplateDesc> = [];
     if (!filenames?.length) {
-      const selected = await multiselect<Array<{ value: string }>, string>({
+      const selected = await multiselect<
+        Array<{ value: TemplateDesc; label: string; hint?: string }>,
+        TemplateDesc
+      >({
         message: "Which templates should be copied?",
-        options: templates.map((value) => ({ value })),
+        options: templates.map((desc) => ({
+          value: desc,
+          label: `${
+            desc.packageManager
+              ? picocolors.italic(picocolors.bold(desc.packageManager + ":"))
+              : ""
+          }${desc.filename}`,
+        })),
       });
       ensureNotCancelled(selected);
-      filenames = selected;
+      processed = selected;
+    } else {
+      processed = filenames.map((filename) => {
+        const [first, second] = filename.split(":");
+        if (second) {
+          if (!supportedManagers.includes(first as SupportedManager))
+            throw new Error(
+              "Unsupported package manager: " + picocolors.red(first)
+            );
+          return {
+            filename: second,
+            packageManager: first as SupportedManager,
+          };
+        } else {
+          return { filename };
+        }
+      });
     }
 
-    let i = filenames.length;
+    const final: Array<TemplateDesc> = [];
+    let i = processed.length;
     while (i--) {
-      const name = filenames[i] ?? "";
-      const valid = templates.includes(name);
-      if (!valid) {
+      const desc = processed[i];
+      if (!desc) continue;
+      const valid = templates.some(
+        (temp) =>
+          temp.filename === desc.filename &&
+          temp.packageManager === desc.packageManager
+      );
+      if (valid) {
+        final.push(desc);
+      } else {
         log.info(
-          picocolors.red(`"${name}"`) +
-            ` is not a valid template and will be ignored`
+          picocolors.red(
+            `"${
+              desc.packageManager
+                ? picocolors.italic(picocolors.bold(desc.packageManager + ":"))
+                : ""
+            }${desc.filename}"`
+          ) + ` is not a valid template and will be ignored`
         );
-        filenames.splice(i, 1);
       }
     }
 
-    for (const template of filenames) {
-      await copyTemplate(template, yes);
+    for (const template of final) {
+      await copyTemplate(template.filename, yes, template.packageManager);
     }
 
     outro("Done");
