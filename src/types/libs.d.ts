2c6c83034083c2aa1fc32335604058dc2cc25a74
Ben Durrant <ben.j.durrant@gmail.com>
2025-03-01 02:01:01 +0000

    fix types

--- src/types/libs.d.ts
+++ src/types/libs.d.ts
@@ -1,5 +1,3 @@
-import "@total-typescript/ts-reset";
-
 declare module "arrgv" {
   function arrgv(string: string): Array<string>;
   export default arrgv;
--- src/types/util.ts
+++ src/types/util.ts
@@ -1,2 +1,4 @@
+import "@total-typescript/ts-reset";
+
 export type NoInfer<T> = [T][T extends any ? 0 : never];
 export type MaybePromise<T> = T | Promise<T>;
